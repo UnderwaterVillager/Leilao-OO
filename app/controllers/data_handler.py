59aa7bd9ee88d5ee50b8f623e01488c491a1735a
vinicius-jesus <vjbfvinicius@hotmail.com>
2025-02-01 15:31:06 -0300

    Define write and read data

--- app/controllers/data_handler.py
+++ app/controllers/data_handler.py
@@ -1,8 +1,34 @@
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, select 
+from sqlalchemy.orm import Session
 
-class QueryDB:
+class DBOperation:
     def __init__(self):
-        self.db = DB()
+        self.engine = create_engine('sqlite:///app.db')
 
-    def get_data(self, query):
-        return self.db.query(query)
+class QueryDB(DBOperation):
+    def __init__(self, table, **filter_params):
+        super().__init__()
+        self.table = table
+        self.filter_params = filter_params
+
+    def query(self):
+        if self.filter_params:
+            with Session(self.engine) as session:
+                query = select(self.table).filter_by(self.filter_params)
+                result = session.execute(query)
+        else:
+            with Session(self.engine) as session:
+                query = select(self.table)
+                result = session.execute(query)
+        return result
+
+class WriteDB(DBOperation):
+    def __init__(self, table, **data):
+        super().__init__()
+        self.table = table
+        self.data = data
+
+    def write(self, data):
+        with Session(self.engine) as session:
+            session.add(self.table(**data))
+            session.commit()
